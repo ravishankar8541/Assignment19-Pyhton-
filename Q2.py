f0e61b18a81860b916cf0b096cb8df37c4bcca10
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-10-23 08:24:53 +0530

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,7 @@
+"""Write a python program to create a function which expects two arguments and print
+them in the function body."""
+
+def f(a,b):
+    print("a =",a)
+    print("b =",b)
+f(7,3)    
